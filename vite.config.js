253045c1e49e34b580b42cc69a5823e7cd202ad4
Tsubasa Yamasaki <tsuba.choko@gmail.com>
2025-08-08 11:12:16 +0900

    古いブラウザへの対応

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,28 @@
 import { defineConfig } from 'vite'
 import { visualizer } from 'rollup-plugin-visualizer';
+import legacy from '@vitejs/plugin-legacy'
 
 
 export default defineConfig({
+  plugins: [
+    legacy({
+      targets: ['chrome >= 50'],
+      additionalLegacyPolyfills: [
+        'regenerator-runtime/runtime',
+        'core-js/modules/es.promise.js',
+        'core-js/modules/es.array.from.js',
+        'core-js/modules/es.array.includes.js',
+        'core-js/modules/es.object.assign.js',
+        'core-js/modules/es.object.entries.js',
+        'core-js/modules/es.object.values.js',
+        'core-js/modules/es.string.includes.js',
+        'core-js/modules/es.string.pad-start.js',
+        'core-js/modules/es.string.pad-end.js'
+      ],
+      polyfills: true,
+      modernPolyfills: false
+    })
+  ],
   server: {
     port: 3000,
     open: true
@@ -11,6 +31,7 @@ export default defineConfig({
     include: ['video.js', 'hls.js', 'shaka-player']
   },
   build: {
+    target: 'es2015',
     rollupOptions: {
       plugins: [visualizer()],
       output: {
